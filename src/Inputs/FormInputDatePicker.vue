c63a4023f0259a2bb0a98ff6ab196b3d369b71b6
tvaliasek <tomas@valiasek.cz>
2021-06-13 12:55:22 +0200

    form input sdate

--- src/Inputs/FormInputDatePicker.vue
+++ src/Inputs/FormInputDatePicker.vue
@@ -2,9 +2,51 @@
     <b-form-group
         :label="label"
         :description="hint"
-        :label-for="id"
+        :label-for="`${id}_input`"
     >
+        <b-input-group v-if="buttonOnly" :class="((invalid !== null) ? ((invalid ? 'is-invalid' : 'is-valid'))  : undefined)">
+            <b-form-input
+                :id="`${id}_input`"
+                v-model.trim="localDate"
+                type="text"
+                :placeholder="datePlaceholder"
+                :size="size"
+                :state="(invalid !== null) ? !invalid : null"
+                :disabled="disabled"
+                :readonly="readOnly"
+                @blur="onTextInputBlur"
+                :formatter="parseFormatDate"
+                lazy-formatter
+            />
+            <b-input-group-append>
+                <b-form-datepicker
+                    :id="id"
+                    :no-flip="true"
+                    :show-decade-nav="true"
+                    :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
+                    :locale="'cs-CZ'"
+                    v-model.trim="model"
+                    :size="size"
+                    :state="(invalid !== null) ? !invalid : null"
+                    :disabled="disabled"
+                    lazy-formatter
+                    :min="minDate"
+                    :max="maxDate"
+                    :start-weekday="1"
+                    v-bind="labels"
+                    :readonly="readOnly"
+                    @change="onEvent('change')"
+                    @update="onEvent('update')"
+                    @blur="onEvent('blur')"
+                    @context="onContext"
+                    :aria-controls="`${id}_input`"
+                    button-only
+                />
+            </b-input-group-append>
+        </b-input-group>
+
         <b-form-datepicker
+            v-else
             :id="id"
             :no-flip="true"
             :show-decade-nav="true"
@@ -39,10 +81,76 @@
 <script>
 import FormInput from './FormInput.vue'
 
+function parseFormatDate (value) {
+    console.log(value)
+    if (value instanceof Date) {
+        return `${value.getDate()}. ${value.getMonth() + 1}. ${value.getFullYear()}`
+    }
+    if (typeof value === 'string') {
+        if (/(\d{4}-\d{2}-\d{2})/ig.test(value.replace(/\s/ig, ''))) {
+            const parts = value.split('-').map(item => parseInt(item))
+            const date = new Date()
+            date.setFullYear(parts[0])
+            date.setMonth(parts[1] - 1)
+            date.setDate(parts[2])
+            return `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()}`
+        }
+        if (/(\d{1,2}\.\d{1,2}\.\d{4})/ig.test(value.replace(/\s/ig, ''))) {
+            const parts = value.split('.').map(item => parseInt(item))
+            const date = new Date()
+            date.setFullYear(parts[2])
+            date.setMonth(parts[1] - 1)
+            date.setDate(parts[0])
+            return `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()}`
+        }
+    }
+    return ''
+}
+
 export default {
     name: 'FormInputDatePicker',
     extends: FormInput,
+    data () {
+        return {
+            formatted: '',
+            selected: ''
+        }
+    },
+    computed: {
+        localDate: {
+            get () {
+                return (this.model) ? parseFormatDate(this.model) : ''
+            },
+            set (value) {
+                const localValue = `${value}`.replace(/\s/ig, '')
+                if (/(\d{1,2}\.\d{1,2}\.\d{4})/ig.test(localValue)) {
+                    const parts = value.split('.').map(item => parseInt(item))
+                    let date = new Date()
+                    date.setFullYear(parts[2])
+                    date.setMonth(parts[1] - 1)
+                    date.setDate(parts[0])
+                    date.setHours(12, 0, 0, 0)
+                    if (this.maxDate instanceof Date && date > this.maxDate) {
+                        date = new Date(this.maxDate.valueOf())
+                    }
+                    if (this.minDate instanceof Date && date < this.minDate) {
+                        date = new Date(this.minDate.valueOf())
+                    }
+                    this.model = date.toISOString().split('T')[0]
+                }
+            }
+        }
+    },
     props: {
+        buttonOnly: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        datePlaceholder: {
+            type: String,
+            required: false
+        },
         minDate: {
             type: Date,
             required: false
@@ -74,6 +182,21 @@ export default {
                     labelTodayButton: 'Vybrat dnešek'
                 }
             }
+        }
+    },
+    methods: {
+        parseFormatDate (value) {
+            return parseFormatDate(value)
+        },
+        onContext (ctx) {
+            this.selected = ctx.selectedYMD
+        },
+        onTextInputBlur () {
+            const value = `${this.model}`
+            this.model = ''
+            this.$nextTick(() => {
+                this.model = value
+            })
         }
     }
 }
