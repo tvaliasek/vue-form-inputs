e3f4fa70b6dcf2453a8400c5fddafc5ce0549e97
tvaliasek <tomas@valiasek.cz>
2023-05-22 12:51:59 +0200

    fix: missing exports

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ import selectInput from './Inputs/FormInputSelect.vue'
 import textareaInput from './Inputs/FormInputTextarea.vue'
 import datePickerInput from './Inputs/FormInputDatePicker.vue'
 import fileInput from './Inputs/FormInputFile.vue'
+import datePickerAltInput from './Inputs/FormInputDatePickerAlt.vue'
 
 export const FormInput = input
 export const FormInputCheckbox = checkboxInput
@@ -38,6 +39,7 @@ export const FormInputSelect = selectInput
 export const FormInputTextarea = textareaInput
 export const FormInputDatePicker = datePickerInput
 export const FormInputFile = fileInput
+export const FormInputDatePickerAlt = datePickerAltInput
 
 export const FormInputsPlugin = (options: { includeBootstrapNextComponents: boolean } = { includeBootstrapNextComponents: true }): Plugin => {
     return {
@@ -73,6 +75,7 @@ export const FormInputsPlugin = (options: { includeBootstrapNextComponents: bool
             app.component('FormInputSelect', FormInputSelect)
             app.component('FormInputDatePicker', FormInputDatePicker)
             app.component('FormInputFile', FormInputFile)
+            app.component('FormInputDatePickerAlt', FormInputDatePickerAlt)
         }
     }
 }
