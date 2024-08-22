5bf5e56a7e29e0ba2f00bc5433ff16d685ef9b10
tvaliasek <tomas@valiasek.cz>
2024-08-22 15:37:17 +0200

    chore: export types

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,14 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
+import { writeFileSync } from 'node:fs'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         dts({
-            tsconfigPath: resolve(__dirname, 'tsconfig.declarations.json')
-        })
+            tsconfigPath: resolve(__dirname, 'tsconfig.declarations.json'),
+            beforeWriteFile: (filePath, content) => {
+                writeFileSync('dist/index.d.mts', content)
+                return {
+                    filePath,
+                    content
+                }
+            }
+        }),
+        visualizer()
     ],
     resolve: {
         alias: {
